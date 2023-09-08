ce430a98daafad8afe02b28af24a2865a4e22fbc
nmm001001 <nmaatnening@gmail.com>
2023-09-08 13:23:45 -0400

    step 5

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -1,13 +1,12 @@
-/* This is a basic file we are using to familiarize ourselves with you! Simply un-comment these
+ /*This is a basic file we are using to familiarize ourselves with you! Simply un-comment these
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+/*Un-comment this and fill in the info*/
+const yourName : String = "Nathan Manning";
+let yourPronouns : String = "He/Him";
+const yourMajor : String = "Computer Science";
+let classification : String = "Junior";
+let oneExcite : String = "Final project";
+let anyConcerns : String = "None";
+let funFact : String = "I enjoy playing the guitar";
